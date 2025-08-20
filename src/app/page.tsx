ec249dccf692ddc89793c803a46dda05375605eb
Lio3307 <aureliofs01@gmail.com>
2025-08-20 17:16:49 +0800

    testing fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import Navbar from "@/components/navbar/Navbar";
+import { prisma } from "./utils/db";
 
-export default function Home() {
+async function getData(){
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      blogId: true,
+      createdAt: true,
+    }
+  })
+  return data;
+}
+
+export default async function Home() {
+
+  const data = await getData();
   return (
-   <>
-   <Navbar/>
-   </>
+    <main>
+      <div className="py-6">
+        <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Post</h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {data.map((item) => (
+            <h1 key={item.title}>{item.title}</h1>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
